2863866ecaac3e7fda5416b4092d4602a1e5f9a9
Nychae <nychae@naver.com>
2021-02-21 10:17:22 +0900

    2.3 map Recap, Add key prop

--- src/App.js
+++ src/App.js
@@ -11,21 +11,25 @@ function Food({ name, picture }) {
 
 const foodILike = [
   {
+    id: 1,
     name: "pizza",
     image:
       "https://post.healthline.com/wp-content/uploads/2020/07/pizza-beer-1200x628-facebook-1200x628.jpg",
   },
   {
+    id: 2,
     name: "cake",
     image:
       "https://igav3-metcdn-com.global.ssl.fastly.net/content/uploads/sites/2/2018/10/16091327/49_Hero.png",
   },
   {
+    id: 3,
     name: "pasta",
     image:
       "https://assets.bonappetit.com/photos/5de7e495b79e20000879d72a/1:1/w_2560%2Cc_limit/Amiel-Lobster-Pasta-Lede-1.jpg",
   },
   {
+    id: 4,
     name: "sandwich",
     image:
       "https://static01.nyt.com/images/2020/11/25/dining/23leftoversrex1/merlin_179868645_ccb9d1b4-9544-4368-afa4-c5fa354aa794-articleLarge.jpg",
@@ -35,7 +39,7 @@ function App() {
   return (
     <div className="App">
       {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
       ))}
     </div>
   );
